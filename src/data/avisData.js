009c1d55658aa5e7c2551b05a4feb73abfc4c7bb
HugoDemont <hugodemont62@hotmail.fr>
2023-01-25 16:41:21 +0100

    Init 25 / 01 / 23

---
+++ src/data/avisData.js
@@ -0,0 +1,41 @@
+/*I want */
+
+var AvisData = [{
+    id: 1,
+    "picture": "https://unsplash.it/1250/800",
+    "avis": "Hugo",
+    "date": "29/06/2016",
+    "comment": "J'ai loué une voiture à l'aéroport de Paris Orly, le personnel était très aimable et très professionnel. La voiture était prête à l'heure et en parfait état. Je recommande vivement Avis."
+}, {
+    id: 2,
+    "picture": "https://unsplash.it/1250/800",
+    "avis": "Virginie",
+    "date": "29/06/2016",
+    "comment": "J'ai loué une voiture à l'aéroport de Paris Orly, le personnel était très aimable et très professionnel. La voiture était prête à l'heure et en parfait état. Je recommande vivement Avis."
+}, {
+    id: 3,
+    "picture": "https://unsplash.it/1250/800",
+    "avis": "May",
+    "date": "29/06/2016",
+    "comment": "J'ai loué une voiture à l'aéroport de Paris Orly, le personnel était très aimable et très professionnel. La voiture était prête à l'heure et en parfait état. Je recommande vivement Avis."
+}, {
+    id: 4,
+    "picture": "https://unsplash.it/1250/800",
+    "avis": "Lucas",
+    "date": "29/06/2016",
+    "comment": "J'ai loué une voiture à l'aéroport de Paris Orly, le personnel était très aimable et très professionnel. La voiture était prête à l'heure et en parfait état. Je recommande vivement Avis."
+}, {
+    id: 5,
+    "picture": "https://unsplash.it/1250/800",
+    "avis": "Alex",
+    "date": "29/06/2016",
+    "comment": "J'ai loué une voiture à l'aéroport de Paris Orly, le personnel était très aimable et très professionnel. La voiture était prête à l'heure et en parfait état. Je recommande vivement Avis."
+}, {
+    id: 6,
+    "picture": "https://unsplash.it/1250/800",
+    "avis": "Lesly",
+    "date": "29/06/2016",
+    "comment": "J'ai loué une voiture à l'aéroport de Paris Orly, le personnel était très aimable et très professionnel. La voiture était prête à l'heure et en parfait état. Je recommande vivement Avis."
+}]
+
+export default AvisData
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,64 +1,98 @@
 import * as React from 'react';
 import {Box} from "@mui/system";
 import {Grid, Typography} from "@mui/material";
-import Hero from '../elements/hero.svg'
-import BullHero from '../elements/bullHero.svg'
-import scroll from '../elements/scroll.svg'
+import Hero from '../elements/hero.svg';
+import BullHero from '../elements/bullHero.svg';
+import scroll from '../elements/scroll.svg';
+import AvisData from "../data/avisData.js";
+import {Splide, SplideSlide} from "@splidejs/react-splide";
+import '@splidejs/react-splide/css';
+import HomeStyle from "../styles/Home.module.css"
+
+const style = HomeStyle
 
 function Home() {
     return (
-        <Box
-            flexDirection="column"
-            display="flex"
-            justifyContent="center"
-            alignItems="center">
-            <Grid container
-                  alignItems='center'
-                  justifyContent="center"
-                  spacing={2}>
+        <>
+            <Box
+                flexDirection="column"
+                display="flex"
+                justifyContent="center"
+                alignItems="center">
+                <Grid container
+                      alignItems='center'
+                      justifyContent="center"
+                      spacing={2}>
+                    <Box
+                        component="img"
+                        alt="Bulle du hero"
+                        src={BullHero}
+                        sx={{
+                            mr: -20,
+                            maxHeight: 400,
+                            maxWidth: 1000,
+                            flexGrow: 1
+                        }}>
+                    </Box>
+                    <Box>
+                        <Typography color="inherit" sx={{fontSize: 100, fontWeight: '700', fontStyle: "italic"}}>
+                            DISCOVER
+                        </Typography>
+                        <Typography color="inherit" sx={{fontSize: 128, fontWeight: '700', fontStyle: "italic"}}>
+                            ECOAL
+                        </Typography>
+                    </Box>
+                    <Box
+                        component="img"
+                        alt="Hero"
+                        src={Hero}
+                        sx={{
+                            maxHeight: 1000,
+                            maxWidth: 1000,
+                            flexGrow: 1
+                        }}>
+                    </Box>
+
+                </Grid>
                 <Box
                     component="img"
-                    alt="Bulle du hero"
-                    src={BullHero}
+                    alt="scroll"
+                    src={scroll}
                     sx={{
-                        mr: -20,
-                        maxHeight: 400,
+                        mt: -22,
+                        maxHeight: 150,
                         maxWidth: 1000,
                         flexGrow: 1
                     }}>
                 </Box>
                 <Box>
-                    <Typography color="inherit" sx={{fontSize: 100, fontWeight: '700', fontStyle: "italic"}}>
-                        DISCOVER
-                    </Typography>
-                    <Typography color="inherit" sx={{fontSize: 128, fontWeight: '700', fontStyle: "italic"}}>
-                        ECOAL
-                    </Typography>
-                </Box>
-                <Box
-                    component="img"
-                    alt="Hero"
-                    src={Hero}
-                    sx={{
-                        maxHeight: 1000,
-                        maxWidth: 1000,
-                        flexGrow: 1
-                    }}>
-                </Box>
+                    {/* Make a slider with the avis and Splide react with 3 avis per 3 avis and have all informations, date, picture avis and comment*/}
+                    <div className={style.cards}>
 
-            </Grid>
-            <Box
-                component="img"
-                alt="scroll"
-                src={scroll}
-                sx={{
-                    mt: -22,
-                    maxHeight: 150,
-                    maxWidth: 1000,
-                    flexGrow: 1
-                }}>
+                        <Splide aria-label="Slider" options={
+                            {
+                                type   : 'loop',
+                                autoplay: true,
+                                rewind: true,
+                                width: 1300,
+                                gap: '1rem'
+                            }}>
+                            {AvisData.map((data, index) => (
+                                <SplideSlide>
+                                    <div className={style.card}>
+                                        <img src={data.picture} alt={index}/>
+                                        <h1>{data.avis}</h1>
+                                        <p>{data.comment}</p>
+                                    </div>
+                                </SplideSlide>
+                            ))}
+                        </Splide>
+                    </div>
+                </Box>
             </Box>
-        </Box>
+
+
+        </>
     )
 }
 
